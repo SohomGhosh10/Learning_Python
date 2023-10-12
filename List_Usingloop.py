af4fac6c31dad43f940bbb199fbddd1867001a65
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-12 22:07:50 +0530

    Add files via upload

---
+++ List_Usingloop.py
@@ -0,0 +1,9 @@
+country = ['India' , 'Pakistan' , 'Australia' , 'Sri lanka']
+total = len(country)
+print(total)
+
+for i in country:
+    print(i)
+    
+#for i in range(len(country)):
+#    print(country[i])    
