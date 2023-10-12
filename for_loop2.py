57c7b773b77825a5f40c6060dbfe1f5571301a3f
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-12 21:41:04 +0530

    Add files via upload

---
+++ for_loop2.py
@@ -0,0 +1,14 @@
+#Reverse order:
+    #for i in range(starting , ending - 1 , -step):
+     #   print(i)
+
+
+#print 20 to 50 with gap 5
+
+for i in range(20 , 51 , 5):
+    print(i,end='')
+
+#print 50 to 20 in reverse order with gap 5
+    
+for i in range(50 , 19 , -5):
+    print(i,end='')
