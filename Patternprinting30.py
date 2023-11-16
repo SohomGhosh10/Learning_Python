b894549c4a876441909c5bac636827d9afa37604
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-11-16 21:50:18 +0530

    Add files via upload

---
+++ Patternprinting30.py
@@ -0,0 +1,7 @@
+h = int(input())
+for row in range(h , 0 , -1):
+    for spaces in range(1 , row  , 1):
+        print(' ',end='')
+    for col in range(h , row - 1, -1):
+        print(chr(col + 64) , end='')
+    print()
