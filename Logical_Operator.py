3aac16161716a8d8f1aab6f3ea5914c6c952f9c6
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-14 22:12:02 +0530

    Update Logical_Operator.py

--- Logical_Operator.py
+++ Logical_Operator.py
@@ -1,3 +1,11 @@
 not True and False or True
 not True and False
 print('1' + str(2)) # Operator Overloading
+
+
+# If 1st condition is false, result depends upon second condition
+# If the first condition is true, then doesn't matter on second result
+
+x = 9
+y = 25
+print(12 == x or y == 20)
