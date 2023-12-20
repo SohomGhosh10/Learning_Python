e59964b2604a64bcf5457d8d8844bf983bdcc021
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-20 23:14:01 +0530

    Update List_append2.py

--- List_append2.py
+++ List_append2.py
@@ -3,6 +3,6 @@ print("Value of n is",x)
 
 storage = []
 for i in range(x):
-    val = int(input())
-    storage.append(val)
+    val = int(input()) 
+    storage.append(val) # addes an element into the list
 print(storage)
