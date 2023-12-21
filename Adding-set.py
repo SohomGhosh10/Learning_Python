47d0190ed46abf5acfb1ca30728276690000305c
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-21 21:05:51 +0530

    Update Adding-set.py

--- Adding-set.py
+++ Adding-set.py
@@ -1,4 +1,4 @@
 x = {10 , 20 , 30 , 40} #adding tuple with set
 y = (50 , 60 , 70 , 80)
 x.add(y)
-print(x)
+print(x) # for adding(), only set and tuple can be added. Singly element can also be added
