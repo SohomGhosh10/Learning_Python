7bfe723a38008d959b6f400eecc9c17cf5fb2ab1
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-06 18:25:32 +0530

    Update Dictonary_Fundamentals.py

--- Dictonary_Fundamentals.py
+++ Dictonary_Fundamentals.py
@@ -1,9 +1,11 @@
 d = {'a':10 , 'b': 20 , 'c': 30 , 'd': 40 , 'e': 50}
 print(d) # {'a': 10, 'b': 20, 'c': 30, 'd': 40, 'e': 50}
 print(d.keys()) # dict_keys(['a', 'b', 'c', 'd', 'e'])
+print(type(d.keys()))
 print(d.values()) # dict_values([10, 20, 30, 40, 50])
+print(type(d.values()))
 print(d.items()) # dict_items([('a', 10), ('b', 20), ('c', 30), ('d', 40), ('e', 50)])
-
+print(type(d.items()))
 
 
 
