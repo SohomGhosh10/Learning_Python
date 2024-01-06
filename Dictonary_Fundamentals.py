ba8856b58ba6a6ea80fb3d2d21931f4b50c7d20c
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-06 18:22:17 +0530

    Add files via upload

---
+++ Dictonary_Fundamentals.py
@@ -0,0 +1,9 @@
+d = {'a':10 , 'b': 20 , 'c': 30 , 'd': 40 , 'e': 50}
+print(d) # {'a': 10, 'b': 20, 'c': 30, 'd': 40, 'e': 50}
+print(d.keys()) # dict_keys(['a', 'b', 'c', 'd', 'e'])
+print(d.values()) # dict_values([10, 20, 30, 40, 50])
+print(d.items()) # dict_items([('a', 10), ('b', 20), ('c', 30), ('d', 40), ('e', 50)])
+
+
+
+
