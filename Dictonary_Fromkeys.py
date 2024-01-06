853f9513ac66dbfe930cdb30fa715cc9a60e71f9
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-06 18:17:51 +0530

    Update Dictonary_Fromkeys.py

--- Dictonary_Fromkeys.py
+++ Dictonary_Fromkeys.py
@@ -1,4 +1,6 @@
 d = {'a':10 , 'b': 20 , 'c': 30 , 'd': 40 , 'e': 50}
 print(d)
-x = dict.fromkeys(d)
-print(x)
+x = dict.fromkeys(d) # Not initialized any value so by default considered 'None'
+y = dict.fromkeys(d , 10) # 10 initialized so every value considered as 10
+print(x) # {'a': None, 'b': None, 'c': None, 'd': None, 'e': None}
+print(y) # {'a': 10, 'b': 10, 'c': 10, 'd': 10, 'e': 10}
