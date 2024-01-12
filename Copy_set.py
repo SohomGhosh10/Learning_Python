9037ea4d258796956a2d67a4ba0c9c7edbf48bf7
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-12 10:29:50 +0530

    Update Copy_set.py

--- Copy_set.py
+++ Copy_set.py
@@ -1,8 +1,8 @@
 x = {10 , 20 , 30 , 40}
 print(x)
-y = set()
+y = set() # initializing a set
 print(y)
-y = x.copy()
-print(y)
-print(id(x))
-print(id(y))
+y = x.copy() # copying 1st set
+print(y) # printing 2nd set
+print(id(x))  # memory address of x
+print(id(y))  # memory address of y
